18dac151dea2fa14c2275ae5c8c67b6a995d1720
lyuehh <lyuehh@gmail.com>
2013-08-01 09:29:36 +0800

    update sed.sh

---
+++ js/25_robot.js
@@ -0,0 +1,183 @@
+var data = [{
+    "ID": 392977,
+    "Title": "COVER",
+    "Content": "",
+    "Url": "http://news.mtime.com/pix/2013/07/26/392977.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/26/185225.22000343_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/26/185225.22000343_160X160.jpg"
+}, {
+    "ID": 391113,
+    "Title": "Top25：《霹雳五号》1986",
+    "Content": "<strong>全球票房：</strong>4069万美元<br /><strong>大卖原因：</strong><span style=\"color:black;\">温情卖萌、未来科技</span><strong><br />机器人角色解读：</strong>很多影迷都认为WALL-E在造型与神态上大胆的向五号致敬，这更说明了霹雳五号的受欢迎程度。非常有意思的是，霹雳五号的设定十分具有80年代的主流时代价值观：反战环保、呼唤人性觉醒，虽然是被研制出来的杀人武器却因为自身的突变而站到了暴力的对立面。<br /><strong>特别性能：</strong>超强记忆力，思维辨析，以及武器系统。",
+    "Url": "http://news.mtime.com/pix/2013/07/19/391113.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/19/142140.76776714_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/19/142140.76776714_160X160.jpg"
+}, {
+    "ID": 391115,
+    "Title": "Top24：《新世纪福音战士新剧场版:破》2009",
+    "Content": "<p><strong>全球票房：</strong>4116万美元<br /><strong>大卖原因：</strong>动漫原作、庞大粉丝<br /><strong>机器人角色解读：</strong>零号机、初代机、二号机一直是《EVA》中的符号式存在，而在该片中三大机体的联袂出战对抗第七使徒，第八使徒，第九使徒，第十使徒的戏码也成为了粉丝们津津乐道的经典场面，当然还有准五号机的意外参战。<br /><strong>特别性能：</strong>超强破坏力、强悍战斗力。</p>",
+    "Url": "http://news.mtime.com/pix/2013/07/19/391115.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/114501.27278477_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/114501.27278477_160X160.jpg"
+}, {
+    "ID": 391117,
+    "Title": "Top23：:《机械战警2》1990",
+    "Content": "<strong>全球票房：</strong>4568万美元<br /><strong>大卖原因：</strong>前集基础<br /><strong>机器人角色解读：</strong>影片给经过机械改造的警察墨菲提出了前所未有的大难题，不仅需要克服机体的物理性背叛，还要用残存的心智力量对是非现状做出判断。这种叠加的矛盾正好对角色本身施加了某种类似于压力式的强制性思考空间，并加强了观众的代入感。<br /><strong>特别性能：</strong>人机改造",
+    "Url": "http://news.mtime.com/pix/2013/07/19/391117.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/19/143623.77381461_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/19/143623.77381461_160X160.jpg"
+}, {
+    "ID": 391116,
+    "Title": "Top22：《机械战警》1987",
+    "Content": "<p><strong>全球票房：</strong>5342万美元<br /><strong>大卖原因：</strong>罪案与机器人的融合、英雄主义<br /><strong>机器人角色解读：</strong>底特律的科学家将已被打死的警察墨菲头脑和机械完美地合二为一，使其成为了一个有着人类头脑和机械身体的机械警察，成了底特律警察的王牌。这个角色的最大魅力在于，其一方面展示出了人们想象中的各种机器人特性，另一方面又散发出人性的光辉与英雄主义的光环；加之导演保罗范霍文写实与粗粝并重的影像质感，使得整部影片更为硬朗饱满，可看度极高。<br /><strong>特别性能：</strong>人机改造</p>",
+    "Url": "http://news.mtime.com/pix/2013/07/19/391116.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/19/143517.74438264_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/19/143517.74438264_160X160.jpg"
+}, {
+    "ID": 392466,
+    "Title": "Top21：《明日世界》2004",
+    "Content": "<p><strong>大卖原因：</strong>蒸汽朋克、巨型机器人兵团<br /><strong>机器人角色解读：</strong>如果说我们介绍了太多形色各异功能众多的机器人的话，那么本片中的机器人则可以称之为最为本质化的初代机械体。那种曾出现于上世纪黄金年代通俗文学中的铁皮大家伙，充满着工业时代的冷硬美感，贯穿着巨大破坏力，影片在设定上为他们加入了一些有趣的科技感，比如远程遥控，这就大大增添了这些机器人的威力以及合理的科学解释。<br /><strong>特别性能：</strong>超强破坏力、巨型化带来的压迫恐惧感。</p>",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392466.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/112316.16147705_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/112316.16147705_160X160.jpg"
+}, {
+    "ID": 392467,
+    "Title": "Top20：《新世纪福音战士新剧场版：Q》2012",
+    "Content": "<strong>大卖原因：</strong>日系ACG经典品牌、全新剧情、奇观展现<br /><strong>机器人角色解读：</strong>由于《EVA》本身的设定，泛用人型决战兵器Evangelion和使徒都成为了独特并新颖的存在，也正因为如此才出现了很多残酷疯狂的激斗场面，人类在维护与改进Evangelion上也付出巨大的精力，虽然他们缺乏自我意识，但是在常年的战斗中已经同驾驶者融为一体，拟人化战斗模式的战力高低也取决于驾驶员的作战思想与意志力。<br /><strong>特别性能：</strong>超强破坏力，强悍战斗力。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392467.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/113319.33162732_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/113319.33162732_160X160.jpg"
+}, {
+    "ID": 392468,
+    "Title": "Top19：《终结者》1984",
+    "Content": "<strong>大卖原因：</strong>全新的机械人设定、科技至上的反思、阿诺·施瓦辛格<br /><strong>机器人角色解读：</strong>相比后两集中充当救世主保护神的T-800，这时的州长是实打实的大反派。不同于那些高精尖的功能机械人反派，州长显得更为原始并充满蛮力，斩尽杀绝破坏到底简直就是精神核心，而那种显露其中的霸气与狠辣恐怕也是后来那几款机械人难以逾越的鸿沟。<br /><strong>特别性能：</strong>会玩时间旅行的硬汉蛮力机器人，披着人皮的金属狂潮。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392468.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/113916.53751515_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/113916.53751515_160X160.jpg"
+}, {
+    "ID": 392507,
+    "Title": "Top18：《机器管家》1999",
+    "Content": "<strong>全球票房：</strong>8742万美元<br /><strong>大卖原因：</strong>丝丝入扣的感情演绎、老戏骨罗宾·威廉斯<br /><strong>机器人角色解读：</strong>机器人安德鲁作为普通家务机器被使用的长久岁月中开始懂得人类的习俗，体会人类的情感，并最终爱上了家中的小女儿。但是苦于没有被安装感觉模块而耽误了这段跨物种的爱恋。直到当他获得模块，安德鲁才体会到什么是吃醋、什么是恋爱。经过一番努力，安德鲁终于完全融入了人类社会。最终，他和自己相爱的人得以长厢厮守，并在垂垂老矣时幸福死去。<br /><strong>特别性能：</strong>管家型家政机器人",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392507.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/141821.38160781_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/141821.38160781_160X160.jpg"
+}, {
+    "ID": 392508,
+    "Title": "Top17：《《复制娇妻》2004",
+    "Content": "<strong>全球票房：</strong>1.02亿美元<br /><strong>大卖原因：</strong>科幻外衣、家庭伦理本质<br /><strong>机器人角色解读：</strong>如何找到一个言听计从上的厨房下的厅堂貌美如花的老婆一定是很多大老爷们儿孜孜不倦的追求，《复制娇妻》为我们结结实实的支了一招儿，但是这个点子的背后却隐藏着无限危机。<br /><strong>特别性能：</strong>温良贤淑、出得厅堂，下得厨房",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392508.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/143922.86619930_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/143922.86619930_160X160.jpg"
+}, {
+    "ID": 392518,
+    "Title": "Top16：《未来战警》2009",
+    "Content": "<strong>大卖原因：</strong>精彩设定<strong>、</strong>CG特效、布鲁斯·威利斯<br /><strong>机器人角色解读：</strong>死宅在家中的人们完全依赖于代理机器人出外进行一切工作活动与日常社交，人们要做的只不过是购买更多的属于自己的机器人，变化他们的外形外貌，而不需要再担心各种问题。从根本上来说，这就好比是将网络世界中的社交通过实体化搬入到现实世界，最终所造成的还是人类之间的隔膜与疏远。<br /><strong>特别性能：</strong>仿真度极高、毫无瑕疵的遥控方式",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392518.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/145704.78921872_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/145704.78921872_160X160.jpg"
+}, {
+    "ID": 392538,
+    "Title": "Top15：《G型神探》1999",
+    "Content": "<strong>全球票房：</strong>1.34亿美元<br /><strong>大卖原因：</strong>各种新奇道具、喜剧元素<br /><strong>机器人角色解读：</strong>将精密复杂的机械嫁接在人体之上做人机结合一直很多科学狂人的梦想，如果说《机械战警》玩的是脱胎换骨的彻底全方位改造，那么《G型神探》则是对半平衡的最佳案例，这样的好处是你一方面可以施展各种超人的本领，一方面还可以插科打诨耍宝泡妞，完全不失人类的酒色财气本质。<br /><strong>特别性能：</strong>旧工业时代的人机概念改造，突出实用功能。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392538.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/150916.94689832_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/150916.94689832_160X160.jpg"
+}, {
+    "ID": 392554,
+    "Title": "Top14：《星际旅行8：第一次接触》1996",
+    "Content": "<strong>全球票房：</strong>1.46亿美元<br /><strong>大卖原因：</strong>星际旅行系列、机械人反派<br /><strong>机器人角色解读：</strong> 《星际旅行》系列中令人望而生畏的反派-博格人是半有机物半机械的生化人，身体上装配有大量人造器官及机械，大脑为人造的处理器。博格人的社会系统由&quot;博格集合体&quot;组成，他们没有自我意识，而是通过一个被称为博格女皇（Borg Queen）的程序对整个集合体进行控制。因此在战略上显示出高度的智慧与适应能力。<br />当然我们还不能忘记片中另外一个经典角色——生化智能机器人&quot;Data&quot;，他也是全片的关键人物。<br /><strong>特别性能</strong>：拥有最先进的科学技术，以及惊人的适应能力。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392554.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/151832.17007483_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/151832.17007483_160X160.jpg"
+}, {
+    "ID": 392595,
+    "Title": "Top13：《环太平洋》2013 仍在上映中",
+    "Content": "<strong>全球票房：</strong>2.24亿美元（截止2013年7月29日）<br /><strong>大卖原因：</strong>吉尔默·德尔·托罗 日本机甲文化 视觉奇观<br /><strong>机器人角色解读：</strong>本片在世界观的设定上非常完整，尤其是对于巨型机械人的分类，研制，发展以及性能。人类先后生产出五代共31台Jaeger机甲，分别来自不同国家，电影主要出现了五款型号（其他基本属于打酱油角色）。中、美、日、俄、澳的机甲战士特色各不同，有的主打神经元双人操控，也有三胞胎合统驾驶，但相同点都是人类在其内部进行控制，这一点类似于EVA等ACG机甲系作品。<br /><strong>特别性能：</strong>神经元网络多人操控，重装火力攻击，近身肉搏，超强格斗技能",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392595.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/093613.18751202_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/093613.18751202_160X160.jpg"
+}, {
+    "ID": 392596,
+    "Title": "Top12：《人工智能》2001",
+    "Content": "<strong>全球票房：</strong>2.35亿<br /><strong>大卖原因：</strong>童星奥斯蒙特、斯皮尔伯格、催泪煽情<br /><strong>机器人角色解读：</strong>假若我们的创造物被赋予了与我们一样的情感与逻辑，智慧与思维，那么他们是不是也应该同我们一样？斯皮尔伯格接过库布里克的钢枪在《人工智能》打算对这一终极迷思做个了结，机器人大卫成了我们追寻答案的最佳切入口，他是一台无法代替人类的机器，一旦失去了利用价值便可以随时被处理掉。然而大卫却拥有自己的完善心智与丰沛情感。<br /><strong>特别性能：</strong>人工智能与自主意识",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392596.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/154714.62231756_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/154714.62231756_160X160.jpg"
+}, {
+    "ID": 392597,
+    "Title": "Top11：《机器人历险记》2005",
+    "Content": "<strong>全球票房：</strong>2.60亿美元<br /><strong>大卖原因：</strong>蓝天工作室动画、机器人城市<br /><strong>机器人角色解读：</strong>影片将人类世界中经常出现的等级划分引入进来，使得我们在看待主角罗德尼这一身份卑微的铜皮机器人时有了先天的屌丝情结，然而这个角色被赋予了积极向上的进取心，其乐观豁达的性格造就了一段传奇佳话，更重要的是它独有的发明头脑与坚定梦想，也令其有了拳脚大展的空间。<br /><strong>特别性能：</strong>发明创造<br />",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392597.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/161110.82075649_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/161110.82075649_160X160.jpg"
+}, {
+    "ID": 392598,
+    "Title": "Top10：《铁甲钢拳》2012",
+    "Content": "<strong>全球票房：</strong>2.95亿美元<br /><strong>大卖原因：</strong>动作捕捉 CG特效 休·杰克曼<br /><strong>机器人角色解读：</strong>《铁甲钢拳》所演绎出的故事正是很多大厂商与游戏迷梦寐以求的游戏操作体验。片中林林总总出现了不同类型不同造型的遥控机器人，千姿百态争奇斗艳但都不如主角父子所操控的&quot;Atom&quot;，谱写了一曲人机屌丝逆袭、父子和好的励志热血佳话。<br /><strong>特别性能：</strong>无与伦比的操控体验，出色的力量与速度，以及各式搏击技巧。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392598.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/114703.74720345_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/114703.74720345_160X160.jpg"
+}, {
+    "ID": 392599,
+    "Title": "Top9：《我，机器人》2004",
+    "Content": "<strong>全球票房：</strong>3.47亿美元<br /><strong>大卖原因：</strong>酷炫的未来世界，各类高科技道具、制作精细的机器、威尔史密斯<br /><strong>机器人角色解读：</strong>尽管阿西莫夫制定了机器人三定律来约束这一族群的根本行动准则，然而科技的进步与人类的野心合二为一，机器人也会诞生出自己的独立思维，而人类又会怎样对待机器人呢？&quot;桑尼&quot;的出现令人惊喜，他聪明谨慎，具备心机却又本质单纯，在一系列的谋杀谜案中试图为自己正名并帮助人类查找真相。<br /><strong>特别性能：</strong>人工智能与自主意识，具备搏击本领与奔跑闪躲能力，以及初级侦查与反侦查技能。<br />",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392599.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/162822.40038691_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/162822.40038691_160X160.jpg"
+}, {
+    "ID": 392618,
+    "Title": "Top8：《终结者2018》2009",
+    "Content": "<strong>全球票房：</strong>3.71亿美元<br /><strong>大卖原因：</strong>终结者兵器大暴走、后天启时代、克里斯蒂安·贝尔<br /><strong>机器人角色解读：</strong>本片的看点是那些形态各异的战斗机器：拥有庞大机身与舱体运输空间并能与大型终结者合体的HK猎杀者飞行器，体积巨大拥有超强破坏能力与进攻能力的收割者机器人，T-105公路摩托赛车终结者，T-103蝎尾终结者，以及T-200，T-300，T-400，T-500等一众人形终结者全部现身，构成了蔚为壮观的天网机器人生产线大观。<br /><strong>特别性能：</strong>各类初代战斗机器人大集结，人形机器人开始登场。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392618.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/170346.49570478_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/170346.49570478_160X160.jpg"
+}, {
+    "ID": 392620,
+    "Title": "Top7：《普罗米修斯》2012",
+    "Content": "<strong>全球票房：</strong>4.03亿美元<br /><strong>大卖原因：</strong>异形前传、雷德利·斯科特<br /><strong>机器人角色解读：</strong>作为探险团队一份子的生化机器人&quot;戴维&quot;，其诸多表现与第一集中的ASH颇为相像。严谨沉郁，人工智能高到几乎拥有属于自己的独立思维，同时又心怀不轨有着不可告人的秘密。戴维是整个影片的重要线索人物，通过他的互动与串联将交织的阴谋与人类的起源尽皆抛出。尤其是作为人造人的身份与人类的创造者接触，象征意味非常浓重，暗示出人类自身的尴尬身份认知。<br /><strong>特别性能：</strong>人工智能与自主意识",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392620.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/171247.65196206_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/171247.65196206_160X160.jpg"
+}, {
+    "ID": 392621,
+    "Title": "Top6：《终结者3》2003",
+    "Content": "<strong>全球票房：</strong>4.33亿美元<br /><strong>大卖原因：</strong>系列基础<strong>、</strong>阿诺·施瓦辛格、美女机器人杀手<br /><strong>机器人角色解读：</strong>本片向我们展示了一种全新的终结者型号T-X，她几乎是一款完美无敌的机器人，除了拥有前任T-1000的液态变形能力之外，还拥有超强的人工智能，手臂火焰枪，高速奔跑能力以及出众的搏击术。更重要的是其与时俱进的配备了赛博网络接入技术，使得在根本上区别与其对峙的T-850。<br /><strong>特别性能：</strong>液态变形能力、超强的人工智能、手臂火焰枪、高速奔跑能力",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392621.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/093403.94793645_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/093403.94793645_160X160.jpg"
+}, {
+    "ID": 392622,
+    "Title": "Top5：《终结者2》1991",
+    "Content": "<strong>全球票房：</strong>5.19亿美元<br /><strong>大卖原因：</strong>前作基础 詹姆斯·卡梅隆 阿诺·施瓦辛格<br /><strong>机器人角色解读：</strong>施瓦辛格所演绎的T-800由于受到了人类的改造，基本遵从了机器人三大定律的行为准则，显得更为忠诚果断，辨识度高，更是创造了经典的机车墨镜皮夹克的硬汉造型；而T-1000冷漠无情，完全程序化的杀人机器式作风则暗喻了未来科技可能的失控沦丧。<br /><strong>特别性能：</strong>液态金属变形自愈模仿能力，高超的拟人态，超强人工智能。难以抵御的力量与破坏力。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392622.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/173324.20431241_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/173324.20431241_160X160.jpg"
+}, {
+    "ID": 392623,
+    "Title": "Top4：《机器人总动员》2008",
+    "Content": "<strong>全球票房：</strong>5.21亿美元<br /><strong>大卖原因：</strong>故事感人、温情卖萌、制作精美<br /><strong>机器人角色解读：</strong>破铁皮，履带动力，一双水灵的大眼睛，孤寂的在地球上执行着日复一日的清扫与垃圾分类任务，却难得的拥有自己的美学信仰与极高的人工智能。<br />WALL-E这个角色在很大程度上代表着一种人类对于自我的放逐跟救赎思潮的折射，其通过使用后工业时代最具有代表性的符号对抗百年孤独，更是在角色的身上注入了黄金时代人文精神的元素，使其与人类本身的价值思维更接近，从而引起人们的思索。<br /><strong>特别性能：</strong>垃圾整理，维修制造技术，飞行能力，少量人工智能与自主意识。",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392623.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/173655.67908471_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/173655.67908471_160X160.jpg"
+}, {
+    "ID": 392624,
+    "Title": "Top3：《变形金刚》2007",
+    "Content": "<p><strong>全球票房：</strong>7.09亿美元<br /><strong>大卖原因：</strong>原著基础、流行文化、必备大视效、机器人情结<br /><strong>机器人角色解读：</strong>该片一共出现13位金刚，汽车人5位，霸天虎8位。汽车人的领袖擎天柱变形为彼得贝奥特379型狗鼻卡车(Peterbilt 379 semi-truck)。而霸天虎的领袖威震天则变形为塞伯坦上的喷气机。<br />对于很多人来讲，这些金刚角色早已是耳熟能详，不过这种三维化后的真人大电影所带来的冲击更为饱满新鲜。尤其是在目睹其变形全过程时那浑身层叠重合变换的金属部件、以及非凡的动作身手后，都会对这些仿佛被动作类型电影硬汉附体的机器人膜拜之极。<br /><strong>特别性能：</strong>高智慧机械生命体，可以扫描任何物件进行拟物变形，拥有强大的武器系统，掌握各种高科技。</p>",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392624.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/174944.12074644_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/24/174944.12074644_160X160.jpg"
+}, {
+    "ID": 392625,
+    "Title": "Top2：《变形金刚2》2009",
+    "Content": "<p><strong>全球票房：</strong>8.36亿美元<br /><strong>大卖原因：</strong>原著基础、流行文化、必备大视效、机器人情结<br /><strong>机器人角色解读：</strong>该片共出现了42位金刚。在《变1》中，擎天柱近身搏斗时其臂管中伸出的橘红色战斧给观众留下了深刻印象，到了《变2》中得以全面升级，擎天柱手持双斧大开杀戒，毫不留情。<br />而威震天在《变2》电影里复活，他身体的一些部件来自一个非自愿捐献的倒霉“挖地虎”成员。在电影里他是三变体，可以变形成辆外星坦克，也可以变成一架外星飞机。其飞机的形态和坦克差不多，只是多了对不显眼的翅膀。<br /><strong>特别性能：</strong>首度出现合体机器人“大力神”，行动能力与间谍能力高超的机器狗。</p>",
+    "Url": "http://news.mtime.com/pix/2013/07/24/392625.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/093111.89742773_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/093111.89742773_160X160.jpg"
+}, {
+    "ID": 392704,
+    "Title": "Top1：《变形金刚3》2011",
+    "Content": "<p><strong>全球票房：</strong>11.23亿美元<br /><strong>大卖原因：</strong>原著基础、流行文化、必备大视效、机器人情结<br /><strong>机器人角色解读：</strong>该片一共出现24位金刚。但与前作不同的是，这次擎天柱终于配备了车厢。除了车型配备了车厢外，擎天柱的人形也有不少变化，比如胸甲及腰部明显与前作不同，手臂处也有所改变。武器方面，除了在前作中多次出现的能量刀以外，这次擎天柱还配备了一把超大型号的机枪、盾、拳套以及战斧。<br />而霸天虎的领袖威震天在《变2》结尾战被“升级”后的擎天柱斩断右臂，并惨遭毁容（右半边脸部被毁），在《变3》里，威震天虽然有了新的手臂，但脸部却没有修复，这给生性残暴的他更添加了一份恐怖感！<br /><strong>特别性能：</strong>高智慧机械生命体</p>",
+    "Url": "http://news.mtime.com/pix/2013/07/25/392704.html",
+    "SmallPicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/092931.54011533_75X75.jpg",
+    "MiddlePicUrl": "http://img31.mtime.cn/CMS/Gallery/2013/07/28/092931.54011533_160X160.jpg"
+}];
--- sed.sh
+++ sed.sh
@@ -21,3 +21,8 @@ js_files=`find . -name "*.js"`
 for i in $js_files; do
   sed -i '' 's/\/js\//\/forfun\/js\//g' $i
 done
+
+git checkout movie/2013/06/02/50-rich-movie-character.html
+git checkout movie/2013/06/21/25-disaster-movies.html
+git checkout movie/2013/06/24/101-us-uk-dramas.html
+git checkout movie/2013/08/01/25-robots-movies.html
