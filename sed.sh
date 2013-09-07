51986bf1f116faf05b94fc9914744c74913b57fc
lyuehh <lyuehh@gmail.com>
2013-09-07 15:50:23 +0800

    update baseurl

--- sed.sh
+++ sed.sh
@@ -6,21 +6,21 @@
 # /movie/ -> /forfun/movie/
 # href="/" -> href="/forfun/"
 
-files=`find . -name "*.html"`
-
-for i in $files; do
-  sed -i '' 's/\/js\//\/forfun\/js\//g' $i
-  sed -i '' 's/\/css\//\/forfun\/css\//g' $i
-  sed -i '' 's/\/movie\//\/forfun\/movie\//g' $i
-  sed -i '' 's/\/life\//\/forfun\/life\//g' $i
-  sed -i '' 's/href=\"\/\"/href=\"\/forfun\/\"/g' $i
-done
-
-js_files=`find . -name "*.js"`
-
-for i in $js_files; do
-  sed -i '' 's/\/js\//\/forfun\/js\//g' $i
-done
+# files=`find . -name "*.html"`
+# 
+# for i in $files; do
+#   sed -i '' 's/\/js\//\/forfun\/js\//g' $i
+#   sed -i '' 's/\/css\//\/forfun\/css\//g' $i
+#   sed -i '' 's/\/movie\//\/forfun\/movie\//g' $i
+#   sed -i '' 's/\/life\//\/forfun\/life\//g' $i
+#   sed -i '' 's/href=\"\/\"/href=\"\/forfun\/\"/g' $i
+# done
+# 
+# js_files=`find . -name "*.js"`
+# 
+# for i in $js_files; do
+#   sed -i '' 's/\/js\//\/forfun\/js\//g' $i
+# done
 
 git checkout movie/2013/06/02/50-rich-movie-character.html
 git checkout movie/2013/06/21/25-disaster-movies.html
